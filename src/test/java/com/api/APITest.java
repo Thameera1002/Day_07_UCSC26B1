ed3a45cbdc415f989686e8b0bebf527dd24f19d8
Thameera Malinga <33679537+Thameera1002@users.noreply.github.com>
2026-04-18 14:07:28 +0530

    APITest added

---
+++ src/test/java/com/api/APITest.java
@@ -0,0 +1,46 @@
+package com.api;
+
+import io.restassured.http.ContentType;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.baseURI;
+
+public class APITest {
+    private String token;
+
+    @BeforeClass
+    public void setup(){
+        baseURI = "https://qa-library.zeoit.com/api/";
+
+        String credentials = "{\n" +
+                "  \"email\": \"super@admin.com\",\n" +
+                "  \"password\": \"password\"\n" +
+                "}";
+
+        token = given()
+                .contentType(ContentType.JSON)
+                .body(credentials)
+                .when()
+                .post("auth/login")
+                .then()
+                .statusCode(200)
+                .extract()
+                .path("token");
+
+        System.out.println(token);
+    }
+
+    @Test
+    public void authAdmin(){
+        given()
+                .header("Authorization","Bearer "+token)
+                .when()
+                .get("auth/me")
+                .then()
+                .statusCode(200)
+                .log().all();
+
+    }
+}
